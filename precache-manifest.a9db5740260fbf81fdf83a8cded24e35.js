0c2c60607910c61eb648df4404a81d856037a706
Jaeruz Datiles <jet.datz@gmail.com>
2020-10-14 21:01:04 +0800

    Updates

---
+++ precache-manifest.a9db5740260fbf81fdf83a8cded24e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab9bff5247d38e6e0a7eb0e875842de",
+    "url": "/react-face-detect/index.html"
+  },
+  {
+    "revision": "9007e61cf18e065e4e43",
+    "url": "/react-face-detect/static/css/main.5929938c.chunk.css"
+  },
+  {
+    "revision": "9619179e1b7db0a72254",
+    "url": "/react-face-detect/static/js/2.3405007b.chunk.js"
+  },
+  {
+    "revision": "d39744b49cd97a6a5d7904677be33b38",
+    "url": "/react-face-detect/static/js/2.3405007b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9007e61cf18e065e4e43",
+    "url": "/react-face-detect/static/js/main.dafc6b93.chunk.js"
+  },
+  {
+    "revision": "81a40ef3342b91a3268d",
+    "url": "/react-face-detect/static/js/runtime-main.ec185de5.js"
+  }
+]);
