5f841a5d8af6a5b853469d5f985c209a6c51886d
Jaeruz Datiles <jet.datz@gmail.com>
2020-10-14 20:58:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect,useRef } from 'react';
+import * as faceapi from 'face-api.js'
+import './App.css'
+
 
 function App() {
+
+  const videoHeight = 480;
+  const videoWidth = 640;
+  const [initializing, setInitializing] = useState(false);
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      setInitializing(true)
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+      ]).then(startVideo)
+    }
+    loadModels();
+  }, [])
+
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({video: {}}).then((stream)=> {videoRef.current.srcObject = stream;}, (err)=> console.error(err));
+  }
+
+  const handleVideoOnPlay = () => {
+    setInterval(async() => {
+      if (initializing) {
+        setInitializing(false)
+      }
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current);
+      const displaySize = {
+        width: videoWidth,
+        height: videoHeight
+      }
+      faceapi.matchDimensions(canvasRef.current, displaySize)
+      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight)
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
+    },10)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{initializing ? 'initializing' : 'Ready'}</span>
+      <div className="display-flex justify-content-center">
+        <video ref={videoRef} autoPlay muted height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} />
+        <canvas ref={canvasRef} className="position-absolute"/> 
+      </div>
+      
     </div>
   );
 }
